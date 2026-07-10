970a0614c4a4464415af8f62404c502a0a3b8de3
Sava-thecoolone <saveliyakov0401@gmail.com>
2026-07-10 18:11:44 +0400

    god damn it i did it wrong

--- SimpleV/run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-call mvnw dependency:build-classpath -Dmdep.outputFile=target\classpath -Dmdep.regenerateFile=true -q
-<NUL set /p=-cp target\classes;> target\cmdargs
-type target\classpath >> target\cmdargs
-title SimpleV
-java @target\cmdargs --add-opens java.base/java.lang=ALL-UNNAMED --add-opens=java.compiler/javax.tools=ALL-UNNAMED --add-opens=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED --add-opens=jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED --add-opens=jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED --add-opens=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED --add-opens=jdk.compiler/com.sun.tools.javac.processing=ALL-UNNAMED --add-opens=jdk.compiler/com.sun.tools.javac.model=ALL-UNNAMED --add-opens=jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED --add-opens=jdk.compiler/com.sun.tools.javac.comp=ALL-UNNAMED --add-opens=jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED simplev.Main %*
