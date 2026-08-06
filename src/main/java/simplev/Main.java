0ece3edb00c62c94bac754fcc29e10d8f9c46052
Sava-thecoolone <saveliyakov0401@gmail.com>
2026-08-06 21:11:41 +0400

    Add files via upload

--- src/main/java/simplev/Main.java
+++ src/main/java/simplev/Main.java
@@ -1,10 +1,11 @@
 package simplev;
 
 import simplev.main.SortVisuailzer;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        SortVisuailzer vis = new SortVisuailzer(args[0]);
+        SortVisuailzer vis = new SortVisuailzer(args[0], Arrays.copyOfRange(args, 1, args.length));
         vis.setupAudio();
         vis.setupRender();
     }
